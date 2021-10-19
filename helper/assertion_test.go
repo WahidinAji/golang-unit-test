bec9e4b81aaafc6a566ba87bb88470cbe2ffacfd
WahidinAji <a17wahidin@gmail.com>
2021-10-19 15:08:43 +0700

    assertions and require test

---
+++ helper/assertion_test.go
@@ -0,0 +1,38 @@
+package helper
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+/*
+assert called Fail()
+require called FailNow()
+ */
+
+func TestHelloWorldAssert(t *testing.T)  {
+	result := HelloWorld("Aji")
+	assert.Equal(t, "Hello Aji",result,"Result must be 'Hello Aji' ")
+	fmt.Println("TestHelloWorld With Assert Done")
+}
+
+func TestHelloWorldAssertFail(t *testing.T)  {
+	result := HelloWorld("Aji Aja")
+	assert.Equal(t, "Hello Aji",result,"Result must be 'Hello Aji' ")
+	fmt.Println("TestHelloWorld With Assert Fail")
+}
+
+//require
+func TestHelloWorldRequire(t *testing.T)  {
+	result := HelloWorld("Aji")
+	require.Equal(t,"Hello Aji",result,"Result must be 'Hello Aji' ")
+	fmt.Println("TestHelloWorld With Require Done") //if failed, this line is never called
+}
+
+func TestHelloWorldRequireFail(t *testing.T)  {
+	result := HelloWorld("Aji Aja")
+	require.Equal(t, "Hello Aji",result,"Result must be 'Hello Aji' ")
+	fmt.Println("TestHelloWorld With Require Fail") //if failed, this line is never called
+}
