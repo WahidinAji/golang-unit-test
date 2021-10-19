32dd9441103e44cad3cb6f73226cbe23ab9551b3
WahidinAji <a17wahidin@gmail.com>
2021-10-19 15:14:27 +0700

    skip test

---
+++ helper/skip_test.go
@@ -0,0 +1,18 @@
+package helper
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"runtime"
+	"testing"
+)
+
+func TestSkip(t *testing.T)  {
+	if runtime.GOOS == "windows"{
+		t.Skip("Can not run on Windows OS")
+	}
+
+	result := HelloWorld("Aji")
+	require.Equal(t,"Hello Aji",result,"Result must be 'Hello Aji' ")
+	fmt.Println("TestHelloWorld With Skip Test Done") //if failed, this line is never called
+}
