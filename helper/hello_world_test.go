3ce1693c895a877a209a308e8511ab5f228bf6fc
WahidinAji <a17wahidin@gmail.com>
2021-10-19 14:54:09 +0700

    unit test basic of funciton or method test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,6 +1,9 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 /*
 before we created unit test, we must know the rules.
@@ -25,9 +28,50 @@ func TestHelloWorld(t *testing.T)  {
 	}
 }
 
-func TestHelloAji(t *testing.T)  {
+
+/*
+if we will to fail result of testing, we can use Fail() and FailNow(),
+Fail() will fail the unit test, but still run to execution the unit test. but in the last if already done, then the unit test will fail.
+FailNow() will fail the unit test right now, without move on to execution the unit test.
+
+Don't use panic for failed unit test.
+ */
+
+func TestHelloWorldFail(t *testing.T)  {
 	result := HelloWorld("Aji")
 	if result != "Hello Aji" {
-		panic("Result is not Hello Aji")
+		t.Fail()
 	}
+	fmt.Println("TestHelloWorldAji Done")
+}
+
+func TestHelloWorldFailNow(t *testing.T)  {
+	result := HelloWorld("Wahidin")
+	if result != "Hello Wahidin" {
+		t.FailNow()
+	}
+	fmt.Println("TestHelloWorldWahidin Done")
+}
+
+/*
+and also, go had Fatal() and Error() to custom the information of your errors test
+Fatal() called FailNow()
+
+and so recommended to use Fatal() and Error(), because we can add information why our information is failed.
+ */
+// Error
+func TestHelloWorldError(t *testing.T)  {
+	result := HelloWorld("Aji")
+
+	if result != "Hello Ajii"{
+		t.Error("Result must be 'Hello Aji' ")
+	}
+	fmt.Println("TestHelloWorldError Done")
+}// Fatal
+func TestHelloWorldFatal(t *testing.T)  {
+	result := HelloWorld("Wahidin")
+	if result != "Hello Wahidinn"{
+		t.Fatal("Result must be 'Hello Wahidin' ")
+	}
+	fmt.Println("TestHelloWorldFatal Done")
 }
