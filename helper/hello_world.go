d557f15ca1b5268cf3219e3cd917a3964a89f991
WahidinAji <a17wahidin@gmail.com>
2021-10-20 21:17:59 +0700

    skip test, main test and sub test

--- helper/hello_world.go
+++ helper/hello_world.go
@@ -7,7 +7,7 @@ we have to include the `test` words in the name file,
 example hello_world_test.go.
 
 besides name file, the name of function still have rules,
-the function in the first character must Capital. like `TestHelloWorld`
+the function in the first character must be Capital. like `TestHelloWorld`
 and must have parameter (t *testing.T)
  */
 
---
+++ helper/main_test.go
@@ -0,0 +1,38 @@
+package helper
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+//before after unit test,
+func TestMain(m *testing.M)  {
+	//before
+	fmt.Println("BEFORE UNIT TEST")
+
+	m.Run()
+
+	//after
+	fmt.Println("AFTER UNIT TEST")
+}
+
+/*
+to run Sub-Test u can use `go test -v -run=TestSubTest/Aji` or `-run=/Aji`. meanwhile, it's also running all subtest
+in other test if it's had the same name (Aji) / (=TestSubTest/Aji).
+ */
+func TestSubTest(t *testing.T)  {
+	//in the below of this line, this is a subTest.
+	t.Run("Aji", func(t *testing.T) {
+		result := HelloWorld("Aji")
+		require.Equal(t,"Hello Aji",result,"Result must be 'Hello Aji' ")
+	})
+	t.Run("Success", func(t *testing.T) {
+		result := HelloWorld("Success")
+		require.Equal(t,"Hello Success",result,"Result must be 'Hello Success' ")
+	})
+	t.Run("Sub Fail", func(t *testing.T) {
+		result := HelloWorld("Sub Fail")
+		require.Equal(t,"Hai Sub Fail",result,"Result must be 'Hello Sub Fail' ")
+	})
+}
